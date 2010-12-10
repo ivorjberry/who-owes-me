1387070f1526b2f96121d364690dfed65e03fcaa
ivorjberry <ivorjberry@b5815a1d-84df-b708-1802-6ea9c075d24a>
2010-12-10 02:10:10 +0000

    started house class

--- WhoOwesMe/src/whoowesme/House.java
+++ WhoOwesMe/src/whoowesme/House.java
@@ -1,12 +1,14 @@
 package whoowesme;
 
 import java.util.*;
-
+import javax.jdo.Transaction;
+import com.google.appengine.api.users.*;
+import javax.jdo.PersistenceManager;
 import javax.jdo.annotations.IdGeneratorStrategy;
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
-
+import java.io.*;
 import com.google.appengine.api.datastore.Key;
 
 @PersistenceCapable
@@ -15,10 +17,33 @@ public class House {
 	@Persistent(valueStrategy= IdGeneratorStrategy.IDENTITY)
 	Key key;
 	
-	@Persistent
-	
+	@Persistent	
 	Vector<Vector<Integer>> houseGrid;
-	List<String> housemates;
+	
+	@Persistent
+	List<User> housemates;
+	
+	@Persistent
+	String housename;
+	
+	public House(String hn){
+		housename = hn;
+	}
+	public void addHousemate(){}
+	public void addBill(User user, User billed, Integer amt, String item){
+		
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		Transaction txn = pm.currentTransaction();
+		try{
+			txn.begin();
+			//put in the servlet??
+			txn.commit();
+		}finally {
+            if (txn.isActive()) {
+                txn.rollback();
+            }
+        }
+	}
 	
 
 }
